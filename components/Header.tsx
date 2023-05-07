af6c8b381241ecd93fb7c1a1aa3a7d708111ba74
amandodias <119734255+amandodias@users.noreply.github.com>
2023-05-07 15:37:46 -0300

    Commit message

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,36 @@
 import type { NextPage } from "next";
 
 type HeaderProps = {
-    setAccessToken(s: string) : void
-}
+  setAccessToken(s: string): void;
+  sair(): void;
+  showModal(): void;
+};
 
-export const Header : NextPage<HeaderProps> = ({setAccessToken}) => {
+export const Header: NextPage<HeaderProps> = ({ setAccessToken, sair, showModal }) => {
+  const mobile = window.innerWidth < 954;
 
-    const mobile = window.innerWidth < 954;
+  const userName = localStorage.getItem("name");
+  const firstName = userName?.split(" ")[0] || "";
 
-    const userName = localStorage.getItem('name');
-    const firstName = userName?.split(' ')[0] || '';
+  const handleSair = () => {
+    localStorage.clear();
+    setAccessToken("");
+  };
 
-    const sair = () => {
-        localStorage.clear();
-        setAccessToken('');
-    }
-
-    return (
-        <div className="container-header">
-            <img src="logo.svg" alt="Logo Fiap" className='logo' />
-            <button><strong>+</strong> Adicionar tarefa</button>
-            <div>
-                <span>Olá, {firstName}</span>
-                <img onClick={sair} src={mobile ? 'exit-mobile.svg' : 'exit-desktop.svg'} alt="Sair"/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="container-header">
+      <img src="/logo.svg" alt="Logo Fiap" className="logo" />
+      <button onClick={showModal}>
+        <span>+</span>Adicionar Tarefa
+      </button>
+      <div className="mobile">
+        <span>Olá, {firstName}</span>
+        <img src="/exit-mobile.svg" alt="Sair" onClick={handleSair} />
+      </div>
+      <div className="desktop">
+        <span>Olá, {firstName}</span>
+        <img src="/exit-desktop.svg" alt="Sair" onClick={handleSair} />
+      </div>
+    </div>
+  );
+};
--- types/Task.ts
+++ types/Task.ts
@@ -2,6 +2,6 @@ export type Task = {
     _id: string | undefined,
     userId: string,
     name: string,
-    finishPrevisionDate: Date,
-    finishDate?: Date
+    finishPrevisionDate: string,
+    finishDate: string
 }
